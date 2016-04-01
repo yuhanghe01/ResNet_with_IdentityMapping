bebd80e066e762bf2c6a369d41d42beb6249e7b4
MichaelHunson <yuhanghe@whu.edu.cn>
2016-04-01 20:15:08 +0800

    second submission

--- residual_net_18_34.py
+++ residual_net_18_34.py
@@ -2,8 +2,8 @@
 @author: Yuhang He 
 @Date: Mar. 26, 2016
 @Email: yuhanghe@whu.edu.cn
-@ReadMe: script generate_residual_net.py and residual_net.py are utilized to create residual net 
-         50/101/152 layer.
+@ReadMe: script generate_residual_net.py and residual_net_18_34.py are utilized to create residual net 
+         18/34 layer.
 @Paper for Reference: Identity Mapping in Deep Residual Networks by Kaiming He et al.
 """
 
@@ -38,7 +38,7 @@ def fc_relu_drop(bottom, num_output=1024, dropout_ratio=0.5):
     return fc, relu, drop
 
 
-def block_conv_bn_scale_relu(bottom, num_output=64, kernel_size=3, stride=1, pad=0):
+def block_conv_bn_scale_relu( bottom, num_output = 64, kernel_size = 3, stride = 1, pad = 0 ):
     conv = L.Convolution(bottom, num_output=num_output, kernel_size=kernel_size, stride=stride, pad=pad,
                          param=[dict(lr_mult=1, decay_mult=1), dict(lr_mult=2, decay_mult=0)],
                          weight_filler=dict(type='xavier', std=0.01),
@@ -67,51 +67,58 @@ def eletwise( bottom1, bottom2 ):
     return eletwise_layer
 
 
-def eltwize_relu(bottom1, bottom2):
-    residual_eltwise = L.Eltwise(bottom1, bottom2, eltwise_param=dict(operation=1))
-    residual_eltwise_relu = L.ReLU(residual_eltwise, in_place=True)
-
-    return residual_eltwise, residual_eltwise_relu
-
-
 def redisual_connect_no_dimen_match( bottom, base_output = 64 ):
     res_i_bn, res_i_scale, res_i_relu = bn_scale_relu( bottom )
     res_i_conv1, res_i_conv1_bn, res_i_conv1_scale, res_i_conv1_relu = \
-        block_conv_bn_scale_relu( res_i_relu, num_output = base_output, kernel_size = 1, stride = 1, pad = 0 )
-    res_i_conv2, res_i_conv2_bn, res_i_conv2_scale, res_i_conv2_relu = \
-        block_conv_bn_scale_relu( res_i_conv1_relu, num_output = base_output, kernel_size = 3, pad = 1 )
-    res_i_conv3 = \
-        conv( res_i_conv2_relu, num_output = 4*base_output, kernel_size = 1, stride = 1, pad = 0 )
+        block_conv_bn_scale_relu( res_i_relu, num_output = base_output, kernel_size = 3, stride = 1, pad = 1 )
+    res_i_conv2 = \
+        conv( res_i_conv1_relu, num_output = base_output, kernel_size = 3, stride = 1, pad = 1 )
 
-    eletwise_layer = eletwise( bottom, res_i_conv3 )
+    eletwise_layer = eletwise( bottom, res_i_conv2 )
     
-    return res_i_bn, res_i_scale, res_i_relu, res_i_conv1, res_i_conv1_bn, res_i_conv1_scale, res_i_conv1_relu, res_i_conv2, res_i_conv2_bn, res_i_conv2_scale, res_i_conv2_relu, res_i_conv3, eletwise_layer
+    return res_i_bn, res_i_scale, res_i_relu, res_i_conv1, res_i_conv1_bn, res_i_conv1_scale, res_i_conv1_relu, res_i_conv2, eletwise_layer
 
 
 def redisual_connect_with_dimen_match( bottom, stride = 1, base_output = 64 ):
     res_i_bn, res_i_scale, res_i_relu = bn_scale_relu( bottom )
-    res_i_conv1, res_i_conv1_bn, res_i_conv1_scale, res_i_conv1_relu = block_conv_bn_scale_relu( res_i_relu, num_output = base_output, kernel_size = 1, stride = stride, pad = 0 )
-    res_i_conv2, res_i_conv2_bn, res_i_conv2_scale, res_i_conv2_relu = block_conv_bn_scale_relu( res_i_conv1_relu, num_output = base_output, kernel_size = 3, pad = 1 )
-    res_i_conv3 = conv( res_i_conv2_relu, num_output = 4*base_output, kernel_size = 1, stride = 1, pad = 0 )
+    res_i_conv1, res_i_conv1_bn, res_i_conv1_scale, res_i_conv1_relu = block_conv_bn_scale_relu( res_i_relu, num_output = base_output, kernel_size = 3, stride = stride + 1, pad = 1 )
+    res_i_conv2 = conv( res_i_conv1_relu, num_output = base_output, kernel_size = 3, stride = 1, pad = 1 )
 
-    bottom_match = conv( bottom, num_output = 4*base_output, kernel_size = 1, stride = stride, pad = 0 )
+    #in the dimension match layer, the stride should be 2
+    res_i_match_conv, res_i_match_bn, res_i_match_scale = block_conv_bn_scale( bottom, num_output = base_output, kernel_size = 1, stride = 2, pad = 0 )
 
-    eletwise_layer = eletwise( bottom_match, res_i_conv3 )
-    return res_i_bn, res_i_scale, res_i_relu, res_i_conv1, res_i_conv1_bn, res_i_conv1_scale, res_i_conv1_relu, res_i_conv2, res_i_conv2_bn, res_i_conv2_scale, res_i_conv2_relu, res_i_conv3, bottom_match, eletwise_layer
+    eletwise_layer = eletwise( res_i_match_scale, res_i_conv2 )
+    return res_i_bn, res_i_scale, res_i_relu, res_i_conv1, res_i_conv1_bn, res_i_conv1_scale, res_i_conv1_relu, res_i_conv2, res_i_match_conv, res_i_match_bn, res_i_match_scale, eletwise_layer
+
+
+def redisual_connect_with_dimen_match_no_patch_reduce( bottom, stride = 1, base_output = 64 ):
+    res_i_bn, res_i_scale, res_i_relu = bn_scale_relu( bottom )
+    res_i_conv1, res_i_conv1_bn, res_i_conv1_scale, res_i_conv1_relu = block_conv_bn_scale_relu( res_i_relu, num_output = base_output, kernel_size = 3, stride = stride, pad = 1 )
+    res_i_conv2 = conv( res_i_conv1_relu, num_output = base_output, kernel_size = 3, stride = stride, pad = 1 )
+
+    res_i_match_conv, res_i_match_bn, res_i_match_scale = block_conv_bn_scale( bottom, num_output = base_output, kernel_size = 1, stride = 1, pad = 0 )
+
+    eletwise_layer = eletwise( res_i_match_scale, res_i_conv2 )
+    return res_i_bn, res_i_scale, res_i_relu, res_i_conv1, res_i_conv1_bn, res_i_conv1_scale, res_i_conv1_relu, res_i_conv2, res_i_match_conv, res_i_match_bn, res_i_match_scale, eletwise_layer
+
 
 
 
 skip_connect_no_dimen_match = 'n.res(stage)_bn, n_res(stage)_scale, n.res(stage)_relu, \
 n.res(stage)_conv1, n.res(stage)_conv1_bn, n.res(stage)_conv1_scale, n.res(stage)_conv1_relu,\
-n.res(stage)_conv2, n.res(stage)_conv2_bn, n.res(stage)_conv2_scale, n.res(stage)_conv2_relu,\
-n.res(stage)_conv3, n.res(stage)_eletwise = \
+n.res(stage)_conv2, n.res(stage)_eletwise = \
 redisual_connect_no_dimen_match( (bottom), base_output = (num) )'
 
 skip_connect_with_dimen_match = 'n.res(stage)_bn, n.res(stage)_scale, n.res(stage)_relu, \
 n.res(stage)_conv1, n.res(stage)_conv1_bn, n.res(stage)_conv1_scale, n.res(stage)_conv1_relu,\
-n.res(stage)_conv2, n.res(stage)_conv2_bn, n.res(stage)_conv2_scale, n.res(stage)_conv2_relu,\
-n.res(stage)_conv3, n.res(stage)_dimen_match, n.res(stage)_eletwise = \
+n.res(stage)_conv2, n.res(stage)_match_conv, n.res(stage)_match_bn, n.res(stage)_match_scale, n.res(stage)_eletwise = \
 redisual_connect_with_dimen_match( (bottom), base_output = (num) )'
+
+skip_connect_with_dimen_match_no_patch_reduce = 'n.res(stage)_bn, n.res(stage)_scale, n.res(stage)_relu, \
+n.res(stage)_conv1, n.res(stage)_conv1_bn, n.res(stage)_conv1_scale, n.res(stage)_conv1_relu,\
+n.res(stage)_conv2, n.res(stage)_match_conv, n.res(stage)_match_bn, n.res(stage)_match_scale, n.res(stage)_eletwise = \
+redisual_connect_with_dimen_match_no_patch_reduce( (bottom), base_output = (num) )'
+
 
 class ResNet(object):
     def __init__(self, lmdb_train, lmdb_test, num_output):
@@ -119,16 +126,7 @@ class ResNet(object):
         self.test_data = lmdb_test
         self.classifier_num = num_output
 
-    def resnet_layers_proto( self, batch_size, phase = 'TRAIN', stages = (3, 4, 6, 3) ):
-        """
-
-        :param batch_size: the batch_size of train and test phase
-        :param phase: TRAIN or TEST
-        :param stages: the num of layers = 2 + 3*sum(stages), layers would better be chosen from [20, 32, 50, 101, 152]
-                       {every stage is composed of 1 residual_branch_shortcut module and stage[i]-1 residual_branch
-                       modules, each module consists of 3 conv layers}
-                        (3, 4, 6, 3) for 50 layers; (3, 4, 23, 3) for 101 layers; (3, 8, 36, 3) for 152 layers
-        """
+    def resnet_layers_proto( self, batch_size, phase = 'TRAIN', stages = (2, 2, 2, 2) ):
         n = caffe.NetSpec()
         if phase == 'TRAIN':
             source_data = self.train_data
@@ -140,14 +138,21 @@ class ResNet(object):
                                  transform_param=dict( crop_size=224, mean_value=[128, 128, 128], mirror = need_mirror))
 
         n.conv1, n.conv1_bn, n.conv1_scale, n.conv1_relu = \
-            block_conv_bn_scale_relu( n.data, num_output = 64, kernel_size = 7, stride = 2, pad = 3 )  # 64x112x112
+            block_conv_bn_scale_relu( n.data, num_output = 64, kernel_size = 7, stride = 2, pad = 3 )
         n.pool1 = L.Pooling( n.conv1, kernel_size = 3, stride = 2, pool = P.Pooling.MAX ) 
        
         residual_num = 0 
         for num in xrange(len(stages)): 
             for i in xrange(stages[num]):
                 residual_num = residual_num + 1
-                if i == 0:
+               
+                if num == 0 and i == 0:
+                  stage_string = skip_connect_with_dimen_match_no_patch_reduce
+                  if residual_num == 1:
+                    bottom_string = 'n.pool1'
+                  else:
+                    bottom_string = 'n.res%s_eletwise'%( str( residual_num -1 ) )
+                elif i == 0 and num > 0:
                     stage_string = skip_connect_with_dimen_match
                     if residual_num == 1:
                         bottom_string = 'n.pool1'
@@ -156,8 +161,7 @@ class ResNet(object):
                 else:
                     stage_string = skip_connect_no_dimen_match
                     bottom_string = 'n.res%s_eletwise'%( str( residual_num -1 ) )
-                
-                exec (stage_string.replace('(stage)', str(residual_num)).replace('(bottom)', bottom_string).replace('(num)', str(2 ** num * 64)))
+                exec (stage_string.replace('(stage)', str(residual_num)).replace('(bottom)', bottom_string).replace('(num)', str( 2 ** num * 64 )))
         
         exec 'n.pool5 = L.Pooling( bottom_string, kernel_size=7, stride=1, pool=P.Pooling.AVE)'.replace('bottom_string', 'n.res%s_eletwise'%str( residual_num ))
         
